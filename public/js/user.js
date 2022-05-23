eeae6b81d04de87cfa775dee0d01bc7ca49438c3
Pavan Kumar V <pavankumarv124@gmail.com>
2022-05-23 20:54:46 +0530

    first-commit

--- public/js/user.js
+++ public/js/user.js
@@ -1,15 +1,8 @@
 $(document).ready(function () {
-  //   $("#uid").on("click", function (e) {
-  //     console.log("invoked");
-  //     e.preventDefault();
-  //     const uid = $("#uidBlovk").find('input[name="uid"]').val();
-  //     console.log(uid);
-  //   });
   $("#userTable table tr td #blockUser").on("click", function (e) {
     e.preventDefault();
     var button = $(this).find("#blockUserBtn");
-    var loader = button.find(".btn-loader").css("display", "block");
-    console.log(loader);
+    var loader = $(this).find(".btn-loader").css("display", "block");
     button.empty();
     button.append(loader);
     var uid = $(this).find("input[name=uid]");
@@ -20,21 +13,43 @@ $(document).ready(function () {
       contentType: "application/json",
       data: JSON.stringify({ uid: uid.val(), active: active.val() }),
       success: function (res) {
-        console.log(res.response);
+        loader.css("display", "none");
         if (res.response == 1) {
+          M.toast({
+            html: ' <span class="bl" style="color: #B8F397;">User Un Blocked<span>',
+            classes: "rounded",
+          });
           active.val("1");
           button.removeClass("bg__success");
           button.addClass("bg__danger");
-          button.html("Block Temporarly");
+          button.append("Block");
         } else {
+          M.toast({
+            html: ' <span class="bl" style="color: #FFDAD4;">User blocked<span> ',
+            classes: "rounded",
+          });
           active.val("0");
           button.removeClass("bg__danger");
           button.addClass("bg__success");
-          button.html("Un Block");
+          button.append(loader);
+          button.append("Un Block");
         }
       },
       error: function (data) {
-        console.log("failed" + data);
+        loader.css("display", "none");
+        M.toast({
+          html: "Operation Failed",
+          classes: "rounded",
+        });
+        if (active.val() == 1) {
+          // button.removeClass("bg__success");
+          // button.addClass("bg__danger");
+          button.append("Block");
+        } else {
+          // button.removeClass("bg__danger");
+          // button.addClass("bg__success");
+          button.append("Un Block");
+        }
       },
     });
   });
